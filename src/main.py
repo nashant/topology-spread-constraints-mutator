7920fb224ddc9cbe5639e8da8b840fe2d2c05d3a
Anthony Nash <anthony.nash@du.co>
2022-03-10 11:45:55 +0000

    Put json spec in annotation

--- src/main.py
+++ src/main.py
@@ -1,17 +1,18 @@
+import logging
 from fastapi import FastAPI, Response, HTTPException
-from base64 import b64encode
-from json import dumps
-from jsonpatch import JsonPatch
+from json import loads
 
-from models import AdmissionReview, Patch, TopologySpreadConstraint, Response as ARResponse
+from models import AdmissionReview, Patch, Response as ARResponse
 from utils import verify_chain
 
 app = FastAPI()
 app.host = "0.0.0.0"
 
+
 @app.get("/")
 async def root():
     return {"message": "Nothing to see"}
+
 
 @app.get("/health", status_code=204, response_class=Response)
 async def health() -> None:
@@ -19,22 +20,20 @@ async def health() -> None:
         return
     raise HTTPException(status_code=400, detail="Certificate chain not valid")
 
+
 @app.post("/mutate")
 async def mutate(body: dict) -> None:
     ar = AdmissionReview(**body)
     pod = ar.request.object
 
-    constraints = []
-    if pod.metadata.annotations is not None:
-        try:
-            constraints.append(TopologySpreadConstraint.fromAnnotations(pod.metadata.annotations))
-        except KeyError:
-            pass
-    patch = [Patch(value=constraints).dict()]
-    patch = str(dumps(patch)).encode()
-    response = {
-        "uid": ar.request.uid,
-        "patch": b64encode(patch).decode()
-    }
-    response = AdmissionReview(response=response)
-    return response
+    spec = pod.metadata.annotations.get("topologySpreadConstraints/spec", None)
+    op = pod.metadata.annotations.get("topologySpreadConstraints/op", "add")
+
+    if spec is None :
+        return AdmissionReview(response={"uid": ar.request.uid, "patchType": None}).json()
+
+    patch = Patch(op=op, value=loads(spec))
+    response = ARResponse(uid=ar.request.uid, patch=patch.dump())
+
+    logging.info(msg=f"Applying patch: {patch}")
+    return AdmissionReview(response=response).json()
--- src/models.py
+++ src/models.py
@@ -1,52 +1,12 @@
 from pydantic import BaseModel
 from typing import Any, Dict, List, Optional
 from enum import Enum
+from base64 import b64encode
 
 
 class Op(str, Enum):
     create = "CREATE"
     update = "UPDATE"
-
-
-class Unsatisfiable(str, Enum):
-    doNotSchedule = "DoNotSchedule"
-    scheduleAnyway = "ScheduleAnyway"
-
-
-class TopologySpreadConstraint(BaseModel):
-    labelSelector: Optional[Dict[str, str]]
-    maxSkew: int
-    topologyKey: str
-    whenUnsatisfiable: Unsatisfiable = Unsatisfiable.doNotSchedule
-
-    @classmethod
-    def fromAnnotations(cls, annotations: Dict[str, str]):
-        """Generate from `topologySpreadConstraint/` prefixed annotations
-
-        Example:
-        topologySpreadConstraint/labelSelector: app=redis,type=master
-        topologySpreadConstraint/maxSkew: 3
-        topologySpreadConstraint/topologyKey: zone
-        topologySpreadConstraint/whenUnsatisfiable: ScheduleAnyway
-        """
-        annotations = {
-            k.lstrip("topologySpreadConstraint/"): v
-            for k, v in annotations.items()
-            if k.startswith("topologySpreadConstraint/")
-        }
-        args = {
-            "labelSelector": annotations["labelSelector"],
-            "maxSkew": int(annotations.get("maxSkew", 1)),
-            "topologyKey": annotations["topologyKey"],
-            "whenUnsatisfiable": annotations.get("whenUnsatisfiable", Unsatisfiable.doNotSchedule)
-        }
-        if args["labelSelector"] != "":
-            labels = args["labelSelector"].split(",")
-            args["labelSelector"] = {
-                l.split("=")[0]: l.split("=")[1]
-                for l in labels
-            }
-        return cls(**args)
 
 
 class Metadata(BaseModel):
@@ -78,11 +38,20 @@ class Request(BaseModel):
     options: Dict[str, str]
 
 
+class Patch(BaseModel):
+    op: str
+    path: str = "/spec/topologySpreadConstraints"
+    value: List[Dict[str, Any]] = []
+
+    def dump(self):
+        return b64encode(f"[{self.json()}]".encode()).decode()
+
+
 class Response(BaseModel):
     allowed: bool = True
-    uid: str
-    patch: str
-    patchtype: str = "JSONPatch"
+    uid: Optional[str]
+    patch: Optional[str]
+    patchtype: Optional[str] = "JSONPatch"
 
 
 class AdmissionReview(BaseModel):
@@ -90,9 +59,3 @@ class AdmissionReview(BaseModel):
     kind: str = "AdmissionReview"
     request: Optional[Request]
     response: Optional[Response]
-
-
-class Patch(BaseModel):
-    op: str = "add"
-    path: str = "/spec/topologySpreadConstraints"
-    value: List[Dict[str, Any]] = []
